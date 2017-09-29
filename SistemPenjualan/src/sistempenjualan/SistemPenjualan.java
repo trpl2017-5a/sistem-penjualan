b6f62e7d6d792bd642d6cc5d8f1516b7c6b6ee9d
tisa <tisa@tisa-PC>
2017-09-29 16:41:11 +0700

    aku nambah sof

--- SistemPenjualan/src/sistempenjualan/SistemPenjualan.java
+++ SistemPenjualan/src/sistempenjualan/SistemPenjualan.java
@@ -17,6 +17,7 @@ public class SistemPenjualan {
     public static void main(String[] args) {
         System.out.println("lallalla");
         System.out.println("tambahLagi");
+        System.out.println("iki nyobak");
     }
     
 }
