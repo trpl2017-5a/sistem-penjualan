851fa6b4fb53753a668914c4c2c9c57292b22fc0
artemtech <sofyanartem@gmail.com>
2017-09-29 16:33:35 +0700

    tambah komentar, tambahlagi

--- SistemPenjualan/src/sistempenjualan/SistemPenjualan.java
+++ SistemPenjualan/src/sistempenjualan/SistemPenjualan.java
@@ -16,6 +16,7 @@ public class SistemPenjualan {
      */
     public static void main(String[] args) {
         System.out.println("lallalla");
+        System.out.println("tambahLagi");
     }
     
 }
