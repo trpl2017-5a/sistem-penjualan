28304f98328fefa126965985f7484e0dc73d120f
artemtech <sofyanartem@gmail.com>
2017-10-01 10:13:03 +0700

    update isi konektor database

--- SistemPenjualan/src/sistempenjualan/model/DBConnector.java
+++ SistemPenjualan/src/sistempenjualan/model/DBConnector.java
@@ -1,10 +1,35 @@
 package sistempenjualan.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author altintop
  */
 public class DBConnector {
+    Connection koneksi;
+    Statement queryStatement;
+
+    public DBConnector() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/multirasa";
+        String username = "aimin";
+        String password = "";
+        
+        this.koneksi = DriverManager.getConnection(url, username, password);
+        this.queryStatement = this.koneksi.createStatement();
+    }
     
+    public void executeQuery(String perintahQuery) throws SQLException{
+        this.queryStatement.execute(perintahQuery);
+    }
+    
+    public ResultSet getResult(String perintahQuery) throws SQLException{
+        return queryStatement.executeQuery(perintahQuery);
+    }
+        
     
 }
