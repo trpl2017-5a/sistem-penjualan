a9293c0bb8151e89204fb08a21dd3bf88f368399
artemtech <sofyanartem@gmail.com>
2017-10-02 11:42:20 +0700

    tambah model produk

---
+++ SistemPenjualan/src/sistempenjualan/model/Produk.java
@@ -0,0 +1,45 @@
+package sistempenjualan.model;
+
+/**
+ *
+ * @author altintop
+ */
+public class Produk {
+    private String idProduk;
+    private String namaProduk;
+    private int harga;
+    private int stok;
+
+    public String getIdProduk() {
+        return idProduk;
+    }
+
+    public void setIdProduk(String idProduk) {
+        this.idProduk = idProduk;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+    
+}
