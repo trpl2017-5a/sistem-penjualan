650bab01ad8fb57b31877581a2c03309647f1a8a
artemtech <sofyanartem@gmail.com>
2017-10-10 06:40:10 +0700

    update main form

--- SistemPenjualan/src/sistempenjualan/Main.java
+++ SistemPenjualan/src/sistempenjualan/Main.java
@@ -5,23 +5,51 @@
  */
 package sistempenjualan;
 
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import sistempenjualan.controller.c_penjualan;
 import sistempenjualan.desain.FrameUtama;
 import sistempenjualan.desain.PanelKasir2;
-import sistempenjualan.model.TransaksiPenjualan;
 
 /**
  *
  * @author USER
  */
 public class Main {
+    
+    private static FrameUtama frameUtama;
+    private static PanelKasir2 panelKasir;
+    
+    public static FrameUtama getFrameUtama(){
+        return frameUtama;
+    }
+    
+    public static PanelKasir2 getPanelKasir(){
+        return panelKasir;
+    }
 
-          public static void main(String[] args) throws SQLException {
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Metal".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrameUtama.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
 
-            c_penjualan l = new c_penjualan();
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                frameUtama = new FrameUtama();
+                frameUtama.setVisible(true);
+            }
+        });
     }
 
 }
